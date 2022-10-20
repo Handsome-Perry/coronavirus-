7862e3c04461e2e6b1a2df225d445a3067a8c433
lipeiyuan <1520529548@qq.com>
2022-10-20 16:35:21 +0800

    CORS

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -3,5 +3,15 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  server: {
+    port: 5173,
+    proxy: {
+      '/api1': {
+        target: 'http://localhost:3000/',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api1/, '') // 不可以省略rewrite
+      }
+    }
+  }
 })
